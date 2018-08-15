9b5ff492422143c2b0c33d7e57324f6af23d3f80
Mehdi Moussa <mehdimoussa.pro@gmail.com>
2018-08-15 17:02:11 +0200

    Yelp MVP

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,23 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # root to: "controller#action"
+
+  # get "restaurants", to: "restaurants#index"
+
+  # get "restaurants/new", to: "restaurants#new"
+  # post "restaurants", to: "restaurants#create"
+
+  # # NB: The `show` route needs to be *after* `new` route.
+  # get "restaurants/:id", to: "restaurants#show", as: :restaurant
+
+
+  # get "restaurants/:restaurant_id/reviews/new", to: "reviews#new"
+  # post "restaurants/:restaurant_id/reviews", to: "reviews#create"
+
+  resources :restaurants, only: [:index, :new, :create, :show]
+
+  resources :restaurants do
+    resources :reviews, only: [:new, :create ]
+  end
+
 end
